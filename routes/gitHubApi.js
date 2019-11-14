018c286d35b7ebc0ca766fdcc13285b8a5099433
MarkusHeine <markusheine.dev@gmail.com>
2019-11-14 20:03:46 +0100

    add endpoint search repos with search term and get repo from owner

--- routes/gitHubApi.js
+++ routes/gitHubApi.js
@@ -1,14 +1,52 @@
 const router = require("express").Router();
 const axios = require("axios");
 
-const endPoint = "https://api.github.com/users";
+const endPoint = "https://api.github.com/";
 
-// Get Repos from User
-
-router.get("/getrepos/:user", async (req, res) => {
+// Get Repos from User by name
+router.get("/getrepos/:owner", async (req, res) => {
     try {
         const resp = await axios.get(
-            `${endPoint}/${req.params.user}/repos`
+            `${endPoint}users/${req.params.owner}/repos`
+        );
+        const repos = JSON.stringify(resp.data);
+        res.json({
+            error: false,
+            repositories: JSON.parse(repos)
+        });
+    } catch (err) {
+        res.json({
+            error: true,
+            errormsg: err
+        })
+    }
+})
+
+// Get repos via search term
+// example querystring: q=tetris&language:javascript&sort=stars&order=desc
+router.get("/searchrepos/:query", async (req, res) => {
+    try {
+        const resp = await axios.get(
+            `${endPoint}search/repositories?${req.params.query}`
+        );
+        const repos = JSON.stringify(resp.data);
+        res.json({
+            error: false,
+            repositories: JSON.parse(repos)
+        });
+    } catch (err) {
+        res.json({
+            error: true,
+            errormsg: err
+        })
+    }
+})
+
+// Get repo from owner
+router.get("/getsinglerepo/:owner/:repo", async (req, res) => {
+    try {
+        const resp = await axios.get(
+            `${endPoint}repos/${req.params.owner}/${req.params.repo}`
         );
         const repos = JSON.stringify(resp.data);
         res.json({
