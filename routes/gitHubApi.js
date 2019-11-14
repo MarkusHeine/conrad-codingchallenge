3dfccadcdb8ccc9f98b9b0dbc8db3f54a527eb38
MarkusHeine <markusheine.dev@gmail.com>
2019-11-14 19:28:18 +0100

    add the getRepos route for requesting the gitHub Api for the repos of a specific user

--- routes/gitHubApi.js
+++ routes/gitHubApi.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+const axios = require("axios");
 
-router.get("/test", (req, res) => {
-    res.send("node server is running");
-});
+const endPoint = "https://api.github.com/users";
+
+// Get Repos from User
+
+router.get("/getrepos/:user", async (req, res) => {
+    try {
+        const resp = await axios.get(
+            `${endPoint}/${req.params.user}/repos`
+        );
+        const repos = JSON.stringify(resp.data);
+        res.json({
+            error: false,
+            repositories: JSON.parse(repos)
+        });
+    } catch (err) {
+        res.json({
+            error: true,
+            errormsg: err
+        })
+    }
+})
 
 module.exports = router;
