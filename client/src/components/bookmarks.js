3c4b7920964343b047af4f8e2bfc0c2ac9cf777f
MarkusHeine <markusheine.dev@gmail.com>
2019-11-15 11:42:22 +0100

    add components and css

---
+++ client/src/components/bookmarks.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./bookmarks.css";
+
+function Bookmarks() {
+  return (
+    <div className="bookmarks-wrapper">
+      <p>bookmarks</p>
+    </div>
+  );
+}
+
+export default Bookmarks;
---
+++ client/src/components/search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./search.css";
+
+function Search() {
+  return (
+    <div className="search-wrapper">
+      <p>search here</p>
+    </div>
+  );
+}
+
+export default Search;
